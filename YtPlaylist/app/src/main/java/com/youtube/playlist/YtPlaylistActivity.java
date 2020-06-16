a1805b89d85739794baf36b73db2e62e0df09c63
CBox-dev <asepmo.story@gmail.com>
2020-06-16 13:51:43 +0700

    Ytplaylist Progress

---
+++ YtPlaylist/app/src/main/java/com/youtube/playlist/YtPlaylistActivity.java
@@ -0,0 +1,114 @@
+package com.youtube.playlist;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.youtube.playlist.R;
+import com.youtube.playlist.adapter.YoutubeVideoAdapter;
+import com.youtube.playlist.model.YoutubeVideoModel;
+import com.youtube.playlist.utils.RecyclerViewOnClickListener;
+import com.youtube.playlist.main.YoutubePlayerActivity;
+
+import java.util.ArrayList;
+
+public class YtPlaylistActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar mToolbar = (Toolbar)findViewById(R.id.toolbar);
+		setSupportActionBar(mToolbar);
+		
+		setUpRecyclerView();
+        populateRecyclerView();
+
+    }
+	
+	@Override
+    public boolean onCreateOptionsMenu(Menu menu)
+	{
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item)
+	{
+		// TODO: Implement this method
+		switch(item.getItemId()){
+			case R.id.action_settings:
+				Intent mYoutube = new Intent(YtPlaylistActivity.this, YoutubeActivity.class);
+				startActivity(mYoutube);
+				break;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+    /**
+     * setup the recyclerview here
+     */
+    private void setUpRecyclerView() {
+        recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+    }
+
+    /**
+     * populate the recyclerview and implement the click event here
+     */
+    private void populateRecyclerView() {
+        final ArrayList<YoutubeVideoModel> youtubeVideoModelArrayList = generateDummyVideoList();
+        YoutubeVideoAdapter adapter = new YoutubeVideoAdapter(this, youtubeVideoModelArrayList);
+        recyclerView.setAdapter(adapter);
+
+        //set click event
+        recyclerView.addOnItemTouchListener(new RecyclerViewOnClickListener(this, new RecyclerViewOnClickListener.OnItemClickListener() {
+            @Override
+            public void onItemClick(View view, int position) {
+
+                //start youtube player activity by passing selected video id via intent
+                startActivity(new Intent(YtPlaylistActivity.this, YoutubePlayerActivity.class).putExtra("video_id", youtubeVideoModelArrayList.get(position).getVideoId()));
+
+            }
+        }));
+    }
+
+
+    /**
+     * method to generate dummy array list of videos
+     *
+     * @return
+     */
+    private ArrayList<YoutubeVideoModel> generateDummyVideoList() {
+        ArrayList<YoutubeVideoModel> youtubeVideoModelArrayList = new ArrayList<>();
+
+        //get the video id array, title array and duration array from strings.xml
+        String[] videoIDArray = getResources().getStringArray(R.array.video_id_array);
+        String[] videoTitleArray = getResources().getStringArray(R.array.video_title_array);
+        String[] videoDurationArray = getResources().getStringArray(R.array.video_duration_array);
+
+        //loop through all items and add them to arraylist
+        for (int i = 0; i < videoIDArray.length; i++) {
+
+            YoutubeVideoModel youtubeVideoModel = new YoutubeVideoModel();
+            youtubeVideoModel.setVideoId(videoIDArray[i]);
+            youtubeVideoModel.setTitle(videoTitleArray[i]);
+            youtubeVideoModel.setDuration(videoDurationArray[i]);
+
+            youtubeVideoModelArrayList.add(youtubeVideoModel);
+
+        }
+
+        return youtubeVideoModelArrayList;
+    }
+}
---
+++ YtPlaylist/app/src/main/java/com/youtube/playlist/listener/Contract.java
@@ -0,0 +1,8 @@
+package com.youtube.playlist.listener;
+
+import com.youtube.playlist.model.Movie;
+
+public interface Contract
+{
+	void onMoview(Movie movie);
+}
